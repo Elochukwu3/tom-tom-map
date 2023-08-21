c10b4706d9fc8caa62737594daf323f6782cfc9a
Elochukwu3 <oguajuromauld5@gmail.com>
2023-08-21 05:55:56 +0100

    clean up

--- src/components/MapCont.tsx
+++ src/components/MapCont.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import * as tt from "@tomtom-international/web-sdk-maps";
-import "@tomtom-international/web-sdk-maps/dist/maps.css"
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import { TMapContent } from "./type";
 import useAddmaker from "./hooks/useAddmaker";
 
@@ -11,34 +11,42 @@ const MapCont = () => {
   const [latitude, setLatitude] = useState<number>(6.5244);
   const [dragedLngLat, setDragedLngLat] = useState<object>({});
   const [longitude, setlongitude] = useState<number>(3.3792);
+  const [loader, setLoader] = useState<Boolean>(true);
+
   const divRef = useRef<HTMLDivElement | null>(null!);
-  const{ addmarker} = useAddmaker({setDragedLngLat, longitude, latitude, element:divRef?.current})
+  const { addmarker } = useAddmaker({
+    setDragedLngLat,
+    longitude,
+    latitude,
+    element: divRef?.current,
+  });
 
   useEffect(() => {
+    const mapOptions: TMapContent = {
+      key: apiKey,
+      container: "map",
+      zoom: 14,
+      center: [longitude, latitude],
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+    };
+    setLoader(false);
+    const map = tt.map(mapOptions);
+    addmarker(map);
+    setMyMap(map);
+    //   console.log(dragedLngLat);
 
-      const mapOptions: TMapContent = {
-        key: apiKey,
-        container: "map",
-        zoom: 14,
-        center:[longitude, latitude],
-        stylesVisibility:{
-            trafficIncidents: true,
-            trafficFlow: true,
-        }
-      };
-      const map = tt.map(mapOptions);
-      setMyMap(myMap);
-      addmarker(map)
-      console.log(dragedLngLat);
-      
-      map.addControl(new tt.FullscreenControl());
-        map.addControl(new tt.NavigationControl());
-  }, [latitude, longitude, dragedLngLat]);
+    map.addControl(new tt.FullscreenControl());
+    map.addControl(new tt.NavigationControl());
+  }, [latitude, longitude]);
+
 
   return (
     <div className="w-full h-full" id="map">
-      <div className="marker" ref={divRef}></div>
-    </div>
+            <div className="marker" ref={divRef}></div>
+          </div>
   );
 };
 
