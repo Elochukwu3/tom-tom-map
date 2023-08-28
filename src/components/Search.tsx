94b04cd3af12ad3487078ce941b2624c5cc9e301
Elochukwu3 <oguajuromauld5@gmail.com>
2023-08-28 20:32:58 +0100

    Add search function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 
 const Search = () => {
   const [input, setInput] = useState("");
   const onchange = (e:any) => {
     setInput(e.target.value);
-  };
-  const onSubmit = (e: FormEventHandler<HTMLFormElement>)=>{
-    e.preventDefault()
-    https://api.tomtom.com/search/2/search/${changedSearchText}.json?
+    // console.log(input);
     
+  };
+
+  const onSubmit = async(e: FormEvent<HTMLFormElement>)=> {
+    e.preventDefault()
+    if(input && input.length > 4){
+      const response = await fetch(`https://api.tomtom.com/search/2/search/${input}.json?key={Your_API_Key}&language=en-US`)
+      console.log(response);
+      
+    }
   }
+    // https://api.tomtom.com/search/2/search/${changedSearchText}.json?
+    // https://api.tomtom.com/search/2/autocomplete/pizza.json?key={Your_API_Key}&language=en-US
+  
+  
   return (
-    <div>
-      Search
-      <form className="flex w-full overflow-hidden" onSubmit={onSubmit}>
+    <div className="rounded-md bg-zinc-800 z-50 md:w-1/2 w-9/12 flex justify-center flex-col items-center fixed right-0 bottom-0">
+      <form className="flex w-10/12 mx-auto bg-white p-0 overflow-hidden" onSubmit={onSubmit}>
+      <button type="submit" className="text-xl font-bold px-2">Q</button>
         <input
           onChange={onchange}
           value={input}
@@ -22,9 +32,13 @@ const Search = () => {
           autoCorrect=""
           autoFocus={true}
           autoComplete=""
+          className="py-2 w-full outline-none text-xl"
         />
-        <button type="submit">Q</button>
+      
       </form>
+      <div className="h-96 w-full  bg-white border-t-2 border-zinc-700 ">
+
+      </div>
     </div>
   );
 };
