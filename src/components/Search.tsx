6b3dd49dc06a8746e7ff792d5714e92a4d749aef
Elochukwu3 <oguajuromauld5@gmail.com>
2023-08-29 01:17:08 +0100

    update api key and base url

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,32 +1,36 @@
 import { useState, FormEvent } from "react";
-const apiKey = import.meta.env.VITE_API_MAP_KEY
+const TOM_TOM_API_KEY = import.meta.env.VITE_API_MAP_KEY;
+const API_URL = `https://api.tomtom.com/search/2/autocomplete/pizza.json?key=${TOM_TOM_API_KEY}&language=en-US`;
 
 const Search = () => {
   const [input, setInput] = useState("");
-  const onchange = (e:any) => {
+  const onchange = (e: any) => {
     setInput(e.target.value);
     // console.log(input);
-    
   };
 
-  const onSubmit = async(e: FormEvent<HTMLFormElement>)=> {
-    e.preventDefault()
-    if(input && input.length > 4){
-      const response = await fetch(`https://api.tomtom.com/search/2/search/${input}.json?key=${apiKey}&language=en-US`)
-      const result = await response.json()
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input && input.length > 4) {
+      const response = await fetch(
+        `https://api.tomtom.com/search/2/search/${input}.json?key=${TOM_TOM_API_KEY}&language=en-US`
+      );
+      const result = await response.json();
       console.log(result);
-      
-      
     }
-  }
-    // https://api.tomtom.com/search/2/search/${changedSearchText}.json?
-    // https://api.tomtom.com/search/2/autocomplete/pizza.json?key={Your_API_Key}&language=en-US
-  
-  
+  };
+  // https://api.tomtom.com/search/2/search/${changedSearchText}.json?
+  // https://api.tomtom.com/search/2/autocomplete/pizza.json?key={Your_API_Key}&language=en-US
+
   return (
     <div className="rounded-md bg-zinc-800 z-50 md:w-1/2 w-9/12 flex justify-center flex-col items-center fixed right-0 bottom-0">
-      <form className="flex w-10/12 mx-auto bg-white p-0 overflow-hidden" onSubmit={onSubmit}>
-      <button type="submit" className="text-xl font-bold px-2">Q</button>
+      <form
+        className="flex w-10/12 mx-auto bg-white p-0 overflow-hidden"
+        onSubmit={onSubmit}
+      >
+        <button type="submit" className="text-xl font-bold px-2">
+          Q
+        </button>
         <input
           onChange={onchange}
           value={input}
@@ -37,11 +41,8 @@ const Search = () => {
           autoComplete=""
           className="py-2 w-full outline-none text-xl"
         />
-      
       </form>
-      <div className="h-96 w-full  bg-white border-t-2 border-zinc-700 ">
-
-      </div>
+      <div className="h-96 w-full  bg-white border-t-2 border-zinc-700 "></div>
     </div>
   );
 };
