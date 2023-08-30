8bf302c118372bf62014b4c14001ad89102bc018
Elochukwu3 <oguajuromauld5@gmail.com>
2023-08-30 22:39:15 +0100

    search jsx mmed

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,37 +2,44 @@ import { useState, FormEvent } from "react";
 import useDebounce from "./hooks/useDbounce";
 const TOM_TOM_API_KEY = import.meta.env.VITE_API_MAP_KEY;
 const API_URL = `https://api.tomtom.com/search/2/autocomplete/pizza.json?key=${TOM_TOM_API_KEY}&language=en-US`;
-import * as searchtt from "@tomtom-international/web-sdk-plugin-searchbox"
+import SearchBox from "@tomtom-international/web-sdk-plugin-searchbox";
+import { services } from "@tomtom-international/web-sdk-services";
 const Search = () => {
-  const [input, setInput] = useState("");		 
+  const [input, setInput] = useState("");
   const [err, setErr] = useState<string | null>(null);
   const debouncevalue = useDebounce(input);
 
-const searchOptions = new searchtt.SearchBox({
-  key:"jjddh",
-  language: 'en-US',
-  minNumberOfCharacters: 2,
+  const apiKey = import.meta.env.VITE_API_MAP_KEY;
 
-})
+  const options = {
+    idleTimePress: 100,
+    minNumberOfCharacters: 0,
+    searchOptions: {
+      key: apiKey,
+      language: "en-GB",
+    },
+    autocompleteOptions: {
+      key: apiKey,
+      language: "en-GB",
+    },
+    noResultsMessage: "No results found.",
+  };
 
+  const ttSearchBox = new SearchBox(services, options);
 
-  const onchange = async(e: any) => {
+  const onchange = async (e: any) => {
     setInput(e.target.value);
-    if(debouncevalue.length > 3){
+    if (debouncevalue.length > 3) {
       console.log(debouncevalue);
-      
-      const response = await fetch(API_URL)
-      const data = await response.json()
+
+      const response = await fetch(API_URL);
+      const data = await response.json();
       console.log(data);
-      
-    try{
 
-    }
-    catch(error){
-
-    }      
-    }else{
-      setErr("Add content")
+      try {
+      } catch (error) {}
+    } else {
+      setErr("Add content");
     }
   };
 
@@ -46,12 +53,17 @@ const searchOptions = new searchtt.SearchBox({
       console.log(result);
     }
   };
+  // tomtom.searchbox.resultselected
+  // console.log(searchBoxHTML);
+
   // https://api.tomtom.com/search/2/search/${changedSearchText}.json?
   // https://api.tomtom.com/search/2/autocomplete/pizza.json?key={Your_API_Key}&language=en-US
 
   return (
     <div className="rounded-md bg-zinc-800 z-50 md:w-1/2 w-9/12 flex justify-center flex-col items-center fixed right-0 bottom-0">
-      <form
+      {/* <searchBoxHTML/> */}
+      <SearchInput  ttSearchBox={ttSearchBox}/>
+      {/* <form
         className="flex w-10/12 mx-auto bg-white p-0 overflow-hidden"
         onSubmit={onSubmit}
       >
@@ -70,7 +82,7 @@ const searchOptions = new searchtt.SearchBox({
           placeholder="Search for places......"
         />
       </form>
-      <div className="h-96 w-full  bg-white border-t-2 border-zinc-700 "></div>
+      <div className="h-96 w-full  bg-white border-t-2 border-zinc-700 "></div> */}
     </div>
   );
 };
